cd61d7466537c2b8a877ef245828db823b760452
MagicJF <joan.forn.codina@gmail.com>
2022-07-07 13:22:09 +0200

    event loaded

--- scraper/launcher.py
+++ scraper/launcher.py
@@ -1,20 +1,26 @@
 import sys
 import json
+from tkinter import PIESLICE
+
+print(sys.argv)
 
 from my_sls_scraper.crawl import crawl
-
 
 def scrape(event={}, context={}):
     print("event== ",event)
     crawl(**event)
 
 if __name__ == "__main__":
+    print(sys.argv)
     try:
+        print("AAA")
         event = json.loads(sys.argv[1])
+
+        print(event)
         print ("This is the name of the script: ", sys.argv[0])
         print ("Number of arguments: ", len(sys.argv))
         print ("The arguments are: " , str(sys.argv))
     except IndexError:
         event = {}
+
     scrape(event)
-    print("event== ",event)
